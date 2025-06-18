a017c7abc862c5828e48ca5c12682fb6fcf2dc50
HenricQ <henricooq@gmail.com>
2025-06-17 21:45:19 -0400

    add estrutura de tabelas

---
+++ src/Dto/UsuarioDto.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Dto;
+
+class UsuarioDto
+{
+    private string $nome;
+    private string $email;
+    private string $telefone;
+    
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+        return $this;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function getTelefone()
+    {
+        return $this->telefone;
+    }
+
+    public function setTelefone($telefone)
+    {
+        $this->telefone = $telefone;
+        return $this;
+    }
+}
